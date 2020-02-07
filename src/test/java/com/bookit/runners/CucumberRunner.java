ffbd24d146f370e8a61a0b9cc16860dfe2270a1d
Aibek-KZ <aibek.risbek@gmail.com>
2020-02-06 17:07:59 -0700

    first commit

---
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -0,0 +1,16 @@
+package com.bookit.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "com/bookit/step_definitions",
+        dryRun = false,
+        tags = "@api and @create_student"
+)
+public class CucumberRunner {
+}
